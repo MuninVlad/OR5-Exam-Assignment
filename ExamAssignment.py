a3d19583a5ebb9c715fd0d98790d0566b7a0ffdc
Luca0022 <568945@student.fontys.nl>
2026-09-24 13:37:39 +0200

    importing

--- ExamAssignment.py
+++ ExamAssignment.py
@@ -1,4 +1,9 @@
 import pandas as pd
-df = pd.read_excel('PaintShop - September 2026.xlsx')
-print(df)
+df_orders = pd.read_excel('PaintShop - September 2026.xlsx', sheet_name= 'Orders')
+df_machines = pd.read_excel('PaintShop - September 2026.xlsx', sheet_name= 'Machines' )
+df_setups = pd.read_excel('PaintShop - September 2026.xlsx', sheet_name= 'Setups' )
+
+print(df_machines)
+
+
 
